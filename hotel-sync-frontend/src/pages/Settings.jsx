1f50bff221d7e031b715eb023f507776b982b0e7
Hasara123 <hasarahithaishi@gmail.com>
2024-10-04 19:07:07 +0530

    Setting Page Added

---
+++ hotel-sync-frontend/src/pages/Settings.jsx
@@ -0,0 +1,18 @@
+// src/pages/Settings.jsx
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Settings = () => {
+  return (
+    <Box sx={{ padding: 3 }}>
+      <Typography variant="h4" gutterBottom>
+        Settings
+      </Typography>
+      {/* Additional Settings Content Here */}
+      <Typography variant="h6">General Settings</Typography>
+      {/* Add more settings options and inputs here */}
+    </Box>
+  );
+};
+
+export default Settings;
